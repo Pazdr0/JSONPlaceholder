a7f2ec8c574073a3d78d33110751431772cebac3
Bartek <barte_x@hotmail.com>
2020-03-18 09:22:00 +0100

    gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,11 @@
 plugins {
     kotlin("jvm") version "1.3.61"
     id("org.openjfx.javafxplugin") version "0.0.8"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
-group = "org.example"
+group = "bgolc.jsonplaceholder"
 version = "1.0-SNAPSHOT"
-
-buildscript {
-    repositories {
-        maven {
-            setUrl("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath("org.openjfx:javafx-plugin:0.0.8")
-    }
-}
-apply(plugin = "org.openjfx.javafxplugin")
 
 javafx {
     version = "11"
@@ -31,9 +20,27 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.google.code.gson:gson:2.8.6")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testCompile("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testCompile("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.4.2")
+    testCompile("org.mockito:mockito-core:2.+")
 }
+
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("org.openjfx:javafx-plugin:0.0.8")
+        classpath("com.github.jengelman.gradle.plugins:shadow:5.2.0")
+    }
+}
+
+apply(plugin = "org.openjfx.javafxplugin")
+apply(plugin = "com.github.johnrengelman.shadow")
 
 tasks {
     compileKotlin {
@@ -41,6 +48,13 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    jar {
+        manifest {
+            attributes(
+                mapOf("Main-Class" to "bgolc.jsonplaceholder.utils.StartGUI")
+            )
+        }
     }
     test {
         useJUnitPlatform()
